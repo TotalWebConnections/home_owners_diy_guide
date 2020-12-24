298792dfa93cba1c155b2f2b4a254b2ce996303a
peterjewicz <peterjewicz@totalwebconnections.com>
2020-12-24 14:11:37 -0600

    Setup base footer

--- src/home_owner/partials/Footer.clj
+++ src/home_owner/partials/Footer.clj
@@ -1,7 +1,19 @@
-(ns home-owner.partials.Footer)
+(ns home-owner.partials.Footer
+  (:require [home-owner.partials.Social :refer [Social]]))
 
 
 (defn Footer []
   [:div.Footer
-   [:p "footer"]
+   [:div.Footer__inner
+    [:div.Footer__inner__nav
+     [:div.Nav__links
+      [:ul
+        [:li [:a {:href "#"} "Home"]]
+        [:li [:a {:href "#"} "About"]]
+        [:li [:a {:href "#"} "Posts"]]
+        [:li [:a {:href "#"} "Contact"]]]]]
+    [:div.Footer__inner__social
+     (Social)]]
+   [:div.Footer__copyright
+    [:p "Copyright 2020 - Total Web Connections LLC"]]
    [:script {:src "https://kit.fontawesome.com/a85405c6c9.js" :crossorigin "anonymous"}]])
