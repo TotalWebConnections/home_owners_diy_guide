00c3759903b81ec09f9c2ec05ca12c39fd738ce9
peterjewicz <peterjewicz@totalwebconnections.com>
2020-12-30 07:55:24 -0600

    About page

--- resources/pages/about.clj
+++ resources/pages/about.clj
@@ -1,7 +1,14 @@
 :header {
-         :title "test"
-         :meta-desc "Meta Descriptionfor about page"}
+         :title "About"
+         :meta-desc "About Homeowners DIY Guide - Your one stop for DIY and home care related content"}
 :page [:div.Page
-        [:h1 "About this site"]
-        [:img {:width "200px" :src "/images/coming_soon.jpg"}]
-        [:p "this is a clojure source file."]]
+        [:h1 "Who We Are"]
+        ; [:img {:width "200px" :src "/images/coming_soon.jpg"}]
+        [:p "DIY Homeowners Guide is a website dedicated to brining you the best in home care content
+             We are passoniate about the upkeep of our homes, and passoniate about doing it ourselves.
+             It's our goal to educated homeowners on common concerns, and help people be more self reliant
+             in taking care of their home."]
+        [:h2 "Write For Us!"]
+        [:p "Interested in writing for us? Drop us an email: info@homeownersdiyguide.com. We're looking
+             for any type of DIY or home improvement related articles. All writing should be high-quality english
+             and not overly promotional. The end goal of our cotent is always a happy reader."]]
