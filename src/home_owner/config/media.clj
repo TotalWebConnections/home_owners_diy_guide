be2e399dca9c6c2d84ac2a57b81861e875c91488
Peter Jewicz <Peterjewicz@totalwebconnections.com>
2021-03-16 12:53:59 -0500

    new article: replace mulch with grass

--- src/home_owner/config/media.clj
+++ src/home_owner/config/media.clj
@@ -11,7 +11,8 @@
    :artificial-grass {:url "artificial-grass.jpg" :alt "Artificial Grass"}
    :how-to-take-care-of-your-gutters {:url "how-to-take-care-of-your-gutters.jpg" :alt "How To Take Care Of Your Gutters"}
    :getting-rid-of-mice {:url "getting-rid-of-mice.jpg" :alt "How To Get Rid Of Mice In Your Home"}
-   :when-to-fertalize-your-lawn {:url "when-to-fertalize-your-lawn.jpg" :alt "When To Fertalize Your Lawn"}})
+   :when-to-fertalize-your-lawn {:url "when-to-fertalize-your-lawn.jpg" :alt "When To Fertalize Your Lawn"}
+   :replace-mulch-with-grass {:url "replace-mulch-with-grass.jpg" :alt "How to replace mulch with grass"}})
 
 (defn get-image-map [key]
   ((keyword key) media))
